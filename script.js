13d0fe1aa19216bb47cf06eeabdd2e16fabc2bbc
mepratikbiswal <153757456+mepratikbiswal@users.noreply.github.com>
2023-12-21 10:12:45 +0530

    Add files via upload
    Early Release

---
+++ script.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".text", {
+    strings: ["Welcome to my Website", "I hope you enjoy your stay"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+function openMailTo() {
+    window.open('mailto:biswalpratik119@gmail.com');
+}
